170f9bea6f721a5a9f75ffd36aab45a6eae59f13
Víctor Campos <portadict@hotmail.com>
2019-12-03 18:10:23 +0100

    Supported empate and pierdes mission outcome

--- shared-lib/missionsUtils.js
+++ shared-lib/missionsUtils.js
@@ -55,6 +55,14 @@ function calcularAtaque({
     throw new Error('Missing params para calcular ataque')
   }
   let resultado
+  let survivingSabots = 0
+  let survivingGuards = 0
+  let gainedFame = 0
+  let destroyedBuildings = 0
+  let incomeForDestroyedBuildings = 0
+  let incomeForRecoveredTroops = 0
+  let attackerTotalIncome = 0
+  let defensorTotalIncome = 0
   // generamos datos
   const guardsAttackPower = personnelList.find(t => t.resource_id === 'guards').attackPower * defensorSecurityLvl // ataque de guardia
   const guardsDefensePower = personnelList.find(t => t.resource_id === 'guards').defensePower * defensorSecurityLvl // defensa de guardia
@@ -85,15 +93,15 @@ function calcularAtaque({
     const realDeadGuards = defensorGuards // muerte guards real
     const realDeadSabots = estimatedDeadSabots // muerte sabots real
 
-    const survivingSabots = attackerSabots - realDeadSabots // sabots vivos
-    const survivingGuards = defensorGuards - realDeadGuards // sabots vivos
+    survivingSabots = attackerSabots - realDeadSabots // sabots vivos
+    survivingGuards = defensorGuards - realDeadGuards // sabots vivos
 
     // Dinero recuperado por tropas muertas
     const recoveredIncomeForDeadGuards = realDeadGuards * guardsPrice // dinero de guards muertos
     const recoveredIncomeForDeadSabots = realDeadSabots * sabotPrice // dinero de sabots muertos
-    const incomeForRecoveredTroops = recoveredIncomeForDeadGuards * 0.1 + recoveredIncomeForDeadSabots * 0.1 // dinero recuperado del ataque
+    incomeForRecoveredTroops = recoveredIncomeForDeadGuards * 0.1 + recoveredIncomeForDeadSabots * 0.1 // dinero recuperado del ataque
 
-    let destroyedBuildings = Math.floor(
+    destroyedBuildings = Math.floor(
       (sabotAttackPower * attackerSabots - guardsDefensePower * defensorGuards) / resistencia
     ) // edificios destruidos
     if (destroyedBuildings > attackedBuilding.maximumDestroyedBuildings) {
@@ -101,28 +109,53 @@ function calcularAtaque({
     } // no superar limites edificios destruidos
     // const remainingBuildings = defensorNumEdificios - destroyedBuildings //edificios restantes
 
-    const gainedFame = destroyedBuildings * attackedBuilding.fame // fama ganada
+    gainedFame = destroyedBuildings * attackedBuilding.fame // fama ganada
 
     const recu = getProfit(edificioID, defensorNumEdificios)
 
     // const buildingsLostIncome = recu * 2 // dinero perdido del edificio
-    const incomeForDestroyedBuildings = recu // dinero recuperado del edificio
+    incomeForDestroyedBuildings = recu // dinero recuperado del edificio
 
     // const pt = recoveredIncomeForDeadGuards + die //perdidas totales defensor
-    const attackerTotalIncome = incomeForRecoveredTroops + incomeForDestroyedBuildings // recuperado total atacante
-    const realAttackerProfit = attackerTotalIncome - recoveredIncomeForDeadSabots // ganancia real atacante
+    attackerTotalIncome = incomeForRecoveredTroops + incomeForDestroyedBuildings // recuperado total atacante
+  } else if (resultado === 'empate') {
+    const realDeadGuards = defensorGuards // muerte guardias real
+    const realDeadSabots = attackerSabots // muerte saboteadores real
 
-    const resultadoAtaque = {
-      resultado,
-      survivingSabots,
-      survivingGuards,
-      gainedFame,
-      destroyedBuildings,
-      incomeForDestroyedBuildings,
-      incomeForRecoveredTroops,
-      attackerTotalIncome,
-      realAttackerProfit,
-    }
-    return resultadoAtaque
+    survivingSabots = attackerSabots - realDeadSabots // saboteadores vivos
+
+    const recoveredIncomeForDeadGuards = realDeadGuards * guardsPrice // dinero de guardias muertos
+    const recoveredIncomeForDeadSabots = realDeadSabots * sabotPrice // dinero de saboteadores muertos
+    incomeForRecoveredTroops = recoveredIncomeForDeadGuards * 0.1 + recoveredIncomeForDeadSabots * 0.1 // dinero recuperado del ataque
+
+    gainedFame = 0 // fama ganada
+
+    // const pt = recoveredIncomeForDeadGuards // perdidas totales defensor
+    attackerTotalIncome = incomeForRecoveredTroops // recuperado total atacante
+  } else if (resultado === 'pierdes') {
+    const realDeadGuards = defensorGuards // muerte guardias real
+    const realDeadSabots = attackerSabots // muerte saboteadores real
+
+    survivingSabots = attackerSabots - realDeadSabots // saboteadores vivos
+
+    const recoveredIncomeForDeadGuards = realDeadGuards * guardsPrice // dinero de guardias muertos
+    const recoveredIncomeForDeadSabots = realDeadSabots * sabotPrice // dinero de saboteadores muertos
+    incomeForRecoveredTroops = recoveredIncomeForDeadGuards * 0.1 + recoveredIncomeForDeadSabots * 0.1 // dinero recuperado del ataque
+
+    defensorTotalIncome = incomeForRecoveredTroops // recuperado total defensor
+
+    gainedFame = destroyedBuildings * attackedBuilding.fame * -1 // fama ganada
   }
+  const resultadoAtaque = {
+    resultado,
+    survivingSabots,
+    survivingGuards,
+    gainedFame,
+    destroyedBuildings,
+    incomeForDestroyedBuildings,
+    incomeForRecoveredTroops,
+    attackerTotalIncome,
+    defensorTotalIncome,
+  }
+  return resultadoAtaque
 }
