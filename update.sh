a993caa8691d7343135ca0fd573467b1fc5da1d8
NiciusB <nuno@balbona.me>
2020-02-03 19:57:32 +0100

    Fix update.sh

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 INITIAL_PATH=$(pwd)
 # Update server
-cd $INITIAL_PATH/packages/game_server
+cd $INITIAL_PATH/packages/game-server
 npm i
 npm run migrate_prod
 pm2 reload all
 # Update client
-cd $INITIAL_PATH/packages/game_client
+cd $INITIAL_PATH/packages/game-client
 npm i
 npm run build
 mkdir -p build_nginx
