2ca1153896c7f56f888e891ebe58dcfc075a2ea9
NiciusB <nuno@balbona.me>
2020-06-08 20:43:50 +0200

    Clean up alliance resources log

--- packages/game-server/src/lib/db/alliances/resources.js
+++ packages/game-server/src/lib/db/alliances/resources.js
@@ -13,10 +13,34 @@ export async function allianceUpdateResource({ type, resourceID, resourceDiff, u
     ])
   }
 
-  // TODO: Clean up history when possible (Ex: multiple consecutive refills)
+  const tsNow = Math.floor(Date.now() / 1000)
+  const tsLimit = tsNow - 60 * 15 // 15min
+  const recentLogForResource = await mysql.selectOne(
+    'SELECT type, quantity FROM alliances_resources_log WHERE alliance_id=? AND user_id=? AND created_at>? AND resource_id=? AND (type="extract" OR type="deposit")',
+    [allianceID, userID, tsLimit, resourceID]
+  )
 
+  if (!recentLogForResource) {
+    await mysql.query(
+      'INSERT INTO alliances_resources_log (alliance_id, user_id, created_at, resource_id, type, quantity) VALUES (?, ?, ?, ?, ?, ?)',
+      [allianceID, userID, tsNow, resourceID, type, Math.abs(resourceDiff)]
+    )
+    return
+  }
+
+  const recentDiff = recentLogForResource.quantity * (recentLogForResource.type === 'extract' ? -1 : 1)
+  const newResourceDiff = resourceDiff + recentDiff
+  if (newResourceDiff === 0) {
+    await mysql.query(
+      'DELETE FROM alliances_resources_log WHERE alliance_id=? AND user_id=? AND created_at>? AND resource_id=? AND (type="extract" OR type="deposit")',
+      [allianceID, userID, tsLimit, resourceID]
+    )
+    return
+  }
+
+  const newType = newResourceDiff < 0 ? 'extract' : 'deposit'
   await mysql.query(
-    'INSERT INTO alliances_resources_log (alliance_id, user_id, created_at, resource_id, type, quantity) VALUES (?, ?, ?, ?, ?, ?)',
-    [allianceID, userID, Math.floor(Date.now() / 1000), resourceID, type, Math.abs(resourceDiff)]
+    'UPDATE alliances_resources_log SET type=?, quantity=?, created_at=? WHERE alliance_id=? AND user_id=? AND created_at>? AND resource_id=? AND (type="extract" OR type="deposit")',
+    [newType, Math.abs(newResourceDiff), tsNow, allianceID, userID, tsLimit, resourceID]
   )
 }
