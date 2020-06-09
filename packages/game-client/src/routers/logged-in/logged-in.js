f70270e34c0f8a110a6de790a2e00b4708a183d4
Nuno Balbona <nuno@balbona.me>
2020-06-10 01:05:05 +0200

    Fix GA tracking again

--- packages/game-client/src/routers/logged-in/logged-in.js
+++ packages/game-client/src/routers/logged-in/logged-in.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import ChatState from 'context/ChatState'
 
 import Header from 'components/header'
 import Footer from 'components/footer'
 import styles from '../style.module.scss'
-import ScrollToTop from 'lib/scrollToTop'
 import ChatBubble from 'components/UI/chat-bubble'
 import ErrorBoundary from 'components/UI/ErrorBoundary'
 
@@ -28,65 +27,62 @@ import Shop from 'routes/shop/shop'
 export default function LoggedInRouter() {
   return (
     <ChatState>
-      <Router>
-        <ScrollToTop />
-        <div className={`${styles.pageContainer} ${styles.loggedIn}`}>
-          <Header />
-          <div className={styles.contentContainer}>
-            <ErrorBoundary>
-              <Switch>
-                <Route path="/buildings">
-                  <Buildings />
-                </Route>
-                <Route path="/research">
-                  <Research />
-                </Route>
-                <Route path="/ranking">
-                  <Ranking />
-                </Route>
-                <Route path="/monopolies">
-                  <Monopolies />
-                </Route>
-                <Route path="/contest">
-                  <Contest />
-                </Route>
-                <Route path="/alliance">
-                  <Alliance />
-                </Route>
-                <Route path="/personnel">
-                  <Personnel />
-                </Route>
-                <Route path="/messages">
-                  <Messages />
-                </Route>
-                <Route path="/finances">
-                  <Finances />
-                </Route>
-                <Route path="/map">
-                  <Map />
-                </Route>
-                <Route path="/newspaper">
-                  <Newspaper />
-                </Route>
-                <Route path="/reports">
-                  <Reports />
-                </Route>
-                <Route path="/settings">
-                  <Settings />
-                </Route>
-                <Route path="/shop">
-                  <Shop />
-                </Route>
-                <Route path="/">
-                  <Home />
-                </Route>
-              </Switch>
-            </ErrorBoundary>
-          </div>
-          <Footer />
+      <div className={`${styles.pageContainer} ${styles.loggedIn}`}>
+        <Header />
+        <div className={styles.contentContainer}>
+          <ErrorBoundary>
+            <Switch>
+              <Route path="/buildings">
+                <Buildings />
+              </Route>
+              <Route path="/research">
+                <Research />
+              </Route>
+              <Route path="/ranking">
+                <Ranking />
+              </Route>
+              <Route path="/monopolies">
+                <Monopolies />
+              </Route>
+              <Route path="/contest">
+                <Contest />
+              </Route>
+              <Route path="/alliance">
+                <Alliance />
+              </Route>
+              <Route path="/personnel">
+                <Personnel />
+              </Route>
+              <Route path="/messages">
+                <Messages />
+              </Route>
+              <Route path="/finances">
+                <Finances />
+              </Route>
+              <Route path="/map">
+                <Map />
+              </Route>
+              <Route path="/newspaper">
+                <Newspaper />
+              </Route>
+              <Route path="/reports">
+                <Reports />
+              </Route>
+              <Route path="/settings">
+                <Settings />
+              </Route>
+              <Route path="/shop">
+                <Shop />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </ErrorBoundary>
         </div>
-        <ChatBubble />
-      </Router>
+        <Footer />
+      </div>
+      <ChatBubble />
     </ChatState>
   )
 }
